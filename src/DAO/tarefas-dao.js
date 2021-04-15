af3050dfa6bafa7229de182a4349f7096ca97b87
MatheusYamasato <matheus-yamasato@hotmail.com>
2021-04-15 20:13:03 -0300

    implementando melhorias no dao

--- src/DAO/tarefas-dao.js
+++ src/DAO/tarefas-dao.js
@@ -31,4 +31,26 @@ module.exports = class TarefasDAO {
             })
         })
     }
+
+    alterarTarefa(tarefa) {
+        return new Promise((res, rej) => {
+            this.bd.run('UPDATE TAREFAS SET (TITULO, DESCRICAO, STATUS) = (?, ?, ?) WHERE (TITULO, DESCRICAO, STATUS) = (?, ?, ?)'
+            , [tarefa.titulo, tarefas.descricao, tarefas.status]
+            , (err) => {
+                if(err) rej('Falha ao alterar a tarefa')
+                else res('Tarefa alterada com sucesso')
+            })
+        })
+    }
+
+    deletarTarefa(tarefa) {
+        return new Promise((res, rej) => {
+            this.bd.run('DELETE FROM TAREFAS WHERE (TITULO) = (?)'
+            , [tarefa.titulo]
+            , (err) => {
+                if(err) rej('Falha ao deletar a tarefa')
+                else res('Tarefa deletada com sucesso')
+            })
+        })
+    }
 }
--- src/DAO/usuarios-dao.js
+++ src/DAO/usuarios-dao.js
@@ -1,5 +1,5 @@
 module.exports = class UsuariosDAO {
-
+    
     constructor(bd) {
         this.bd = bd;
     }
@@ -23,11 +23,30 @@ module.exports = class UsuariosDAO {
             this.bd.run('INSERT INTO USUARIOS (NOME, EMAIL, SENHA) VALUES (?,?,?)'
             , [usuario.nome, usuario.email, usuario.senha]
             , (err) => {
-                if(err) {
-                    rej('Falha ao inserir usuário')
-                } else {
-                    res('Usuário inserido com sucesso')
-                }
+                if(err) rej('Falha ao inserir usuário')
+                else res('Usuário inserido com sucesso')
+            })
+        })
+    }
+
+    alterarUsuario(usuario) {
+        return new Promise((res, rej) => {
+            this.bd.run('UPDATE USUARIOS SET (NOME, EMAIL, SENHA) = (?, ?, ?) WHERE (NOME, EMAIL SENHA) = (?, ?, ?)'
+            , [usuario.nome, usuario.email, usuario.senha]
+            , (err) => {
+                if(err) rej('Falha ao alterar o usuário')
+                else res('Usuário alterado com sucesso')
+            })
+        })
+    }
+
+    deletarUsuario(usuario) {
+        return new Promise((res, rej) => {
+            this.bd.run('DELETE FROM USUARIOS WHERE (NOME) = (?)'
+            , [usuario.nome]
+            , (err) => {
+                if(err) rej('Falha ao deletar o usuário')
+                else res('Usuário deletado com sucesso')
             })
         })
     }
